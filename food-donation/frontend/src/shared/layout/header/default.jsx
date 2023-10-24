dc4aecbda5012e4e24c80f9ebd0a7021e17a25f4
Mahinthan Kishanthan <87487677+IT21150166@users.noreply.github.com>
2023-10-24 14:10:03 +0530

    Add files via upload

---
+++ food-donation/frontend/src/shared/layout/header/default.jsx
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react'
+import { AppBar, Box, IconButton, LinearProgress, Toolbar, Typography } from '@mui/material'
+import { AppContext } from '../../../context/AppContext'
+import LogoutIcon from '@mui/icons-material/Logout';
+
+const Header = () => {
+    const {appLoading, user, logout} = useContext(AppContext);
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar sx={{backgroundColor:"green"}}>
+                    <Box maxWidth="xl" mx="auto" sx={{ display: 'flex', justifyContent: 'space-between', width: '100%', alignItems: 'center' }}>
+                        <Typography 
+                        component="div" 
+                        sx={{ 
+                            fontSize:24,
+                            fontWeight:700,
+
+                        }}
+                        >
+                        FOOD DONOR
+                        </Typography>
+                        {user&& (
+                            <IconButton onClick={logout} >
+                                <LogoutIcon sx={{color:'white'}} />
+                            </IconButton>
+                        )}
+                    </Box>
+                </Toolbar>
+                {appLoading && (
+                    <LinearProgress />
+                )}
+            </AppBar>
+        </Box> 
+  )
+}
+
+export default Header;
